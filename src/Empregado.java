9d6004b1f502701446090cacb61760f7801518b7
thiago <thiago.alves@ifce.edu.br>
2023-01-13 20:25:22 -0300

    cria atributo ultimo nome em Empregado e atualiza os gets e sets

--- src/Empregado.java
+++ src/Empregado.java
@@ -1,9 +1,12 @@
 public class Empregado {
-	private String nome;
+	private String primeiroNome;
+	private String ultimoNome;
 	private double salarioMensal;
 
-	public Empregado(String nome, double salarioMensal){
-		this.nome = nome;
+	public Empregado(String primeiroNome, String ultimoNome,
+					 double salarioMensal){
+		this.primeiroNome = primeiroNome;
+		this.ultimoNome = ultimoNome;
 		if (salarioMensal > 0){
 			this.salarioMensal = salarioMensal;
 		}
@@ -12,12 +15,20 @@ public class Empregado {
 		}
 	}
 
-	public String getNome(){
-		return nome;
+	public String getPrimeiroNomeNome(){
+		return primeiroNome;
 	}
 
-	public void setNome(String novoNome){
-		nome = novoNome;
+	public void setPrimeiroNomeNome(String novoNome){
+		primeiroNome = novoNome;
+	}
+
+	public String getUltimoNome(){
+		return ultimoNome;
+	}
+
+	public void setUltimoNome(String ultimoNome){
+		this.ultimoNome = ultimoNome;
 	}
 
 	public double getSalarioMensal(){
