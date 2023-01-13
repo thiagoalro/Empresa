f5a6460d819c7ff260e9bac77ff5e37a029b9235
thiago <thiago.alves@ifce.edu.br>
2023-01-13 19:22:49 -0300

    cria classe Empregado

---
+++ src/Empregado.java
@@ -0,0 +1,12 @@
+public class Empregado {
+	private String nome;
+	private double salarioMensal;
+
+	public String getNome(){
+		return nome;
+	}
+
+	public void setNome(String novoNome){
+		nome = novoNome;
+	}
+}
