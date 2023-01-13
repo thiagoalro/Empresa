788487a989d286a9643cb08459d8c30cb3eb1b7b
thiago <thiago.alves@ifce.edu.br>
2023-01-13 20:32:32 -0300

    adiciona os ultimos nomes nos prints do main

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,15 @@ public class Main {
 				"Sousa",
 				2745.01);
 
-		System.out.printf("Nome do primeiro funcionário: %s%n",
-				empregado1.getPrimeiroNome());
+		System.out.printf("Nome do primeiro funcionário: %s %s%n",
+				empregado1.getPrimeiroNome(), empregado1.getUltimoNome());
 		System.out.printf("Salário do primeiro funcionário: %.2f%n",
 				empregado1.getSalarioMensal());
 		System.out.printf("Ganhos anuais do primeiro funcionário: " +
 				"%.2f%n", empregado1.getSalarioMensal() * 12);
 
-		System.out.printf("Nome do segundo funcionário: %s%n",
-				empregado2.getPrimeiroNome());
+		System.out.printf("Nome do segundo funcionário: %s %s%n",
+				empregado2.getPrimeiroNome(), empregado2.getUltimoNome());
 		System.out.printf("Salário do segundo funcionário: %.2f%n",
 				empregado2.getSalarioMensal());
 		System.out.printf("Ganhos anuais do segundo funcionário: " +
@@ -24,16 +24,16 @@ public class Main {
 		System.out.println("\n********** Depois do aumento de 10% *****************\n");
 
 		empregado1.setSalarioMensal(empregado1.getSalarioMensal() * 1.1);
-		System.out.printf("Nome do primeiro funcionário: %s%n",
-				empregado1.getPrimeiroNome());
+		System.out.printf("Nome do primeiro funcionário: %s %s%n",
+				empregado1.getPrimeiroNome(), empregado1.getUltimoNome());
 		System.out.printf("Salário do primeiro funcionário: %.2f%n",
 				empregado1.getSalarioMensal());
 		System.out.printf("Ganhos anuais do primeiro funcionário: " +
 				"%.2f%n", empregado1.getSalarioMensal() * 12);
 
 		empregado2.setSalarioMensal(empregado2.getSalarioMensal() * 1.1);
-		System.out.printf("Nome do segundo funcionário: %s%n",
-				empregado2.getPrimeiroNome());
+		System.out.printf("Nome do segundo funcionário: %s %s%n",
+				empregado2.getPrimeiroNome(), empregado2.getUltimoNome());
 		System.out.printf("Salário do segundo funcionário: %.2f%n",
 				empregado2.getSalarioMensal());
 		System.out.printf("Ganhos anuais do segundo funcionário: " +
