dc5dc908b6f2547149e327a4ca50e60bd58a5549
thiago <thiago.alves@ifce.edu.br>
2023-01-13 19:59:25 -0300

    aumenta em 10% os salários dos dois empregados

--- src/Main.java
+++ src/Main.java
@@ -23,6 +23,24 @@ public class Main {
 		System.out.printf("Ganhos anuais do segundo funcionário: " +
 				"%.2f%n", empregado2.getSalarioMensal() * 12);
 
+		System.out.println("\n********** Depois do aumento de 10% *****************\n");
+
+		empregado1.setSalarioMensal(empregado1.getSalarioMensal() * 1.1);
+		System.out.printf("Nome do primeiro funcionário: %s%n",
+				empregado1.getNome());
+		System.out.printf("Salário do primeiro funcionário: %.2f%n",
+				empregado1.getSalarioMensal());
+		System.out.printf("Ganhos anuais do primeiro funcionário: " +
+				"%.2f%n", empregado1.getSalarioMensal() * 12);
+
+		empregado2.setSalarioMensal(empregado2.getSalarioMensal() * 1.1);
+		System.out.printf("Nome do segundo funcionário: %s%n",
+				empregado2.getNome());
+		System.out.printf("Salário do segundo funcionário: %.2f%n",
+				empregado2.getSalarioMensal());
+		System.out.printf("Ganhos anuais do segundo funcionário: " +
+				"%.2f%n", empregado2.getSalarioMensal() * 12);
+
 
 	}
 }
